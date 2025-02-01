9355add3b48ed0ce87980e50523b1f75f0ac5bfd
jhpark0888 <jhpark0888@naver.com>
2025-02-01 21:16:24 +0900

    # EDIT - 카드 구매하기(11052) 속도 최적화

--- dynamic_programming/base/11052.swift
+++ dynamic_programming/base/11052.swift
@@ -3,9 +3,11 @@ var dp = readLine()!.split(separator: " ").map { Int($0)! }
 dp.insert(0, at: 0)
 
 for i in 2...n {
-    for j in 1...(i / 2) {
-        dp[i] = max(dp[i], dp[j] + dp[i - j])
+    var maxValue = 0
+    for j in stride(from: i, through: i / 2, by: -1) {
+        maxValue = max(maxValue, dp[j] + dp[i - j])
     }
+    dp[i] = maxValue
 }
 
 print(dp[n])
