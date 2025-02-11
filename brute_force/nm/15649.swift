3f83cdfc68996396d1b5718f2d36cec47ae55d55
jhpark0888 <jhpark0888@naver.com>
2025-02-11 22:02:53 +0900

    # SOLVE - N과 M (1)(15649)

---
+++ brute_force/nm/15649.swift
@@ -0,0 +1,25 @@
+let nm = readLine()!.split(separator: " ").map { Int($0)! }
+let n = nm[0], m = nm[1]
+var visited = Array(repeating: false, count: n + 1)
+
+func back_tracking(stack: [Int]) {
+    if stack.count == m {
+        for s in stack {
+            print(s, terminator: " ")
+        }
+        print()
+        return
+    }
+    
+    for i in 1...n {
+        if !visited[i] {
+            visited[i] = true
+            back_tracking(stack: stack + [i])
+            visited[i] = false
+        }
+    }
+}
+
+back_tracking(stack: [])
+
+// N과 M (1)
