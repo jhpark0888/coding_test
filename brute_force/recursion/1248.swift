3e9fa4c5884149bd0de3777559cff2bf058bd609
jhpark0888 <jhpark0888@naver.com>
2025-02-19 21:44:37 +0900

    # EDIT - 1248 풀이 개선

--- brute_force/recursion/1248.swift
+++ brute_force/recursion/1248.swift
@@ -1,69 +1,110 @@
+// let n = Int(readLine()!)!
+// let sign: [Character] = Array(readLine()!)
+// var signMatrix: [[Character]] = [] 
+// var curIndex = 0
+// var result = [Int]()
+
+// for i in stride(from: n, through: 1, by: -1) {
+//     signMatrix.append(Array(sign[curIndex..<curIndex + i]))
+//     curIndex += i
+// }
+
+// func checkSignMatrix(sequence: [Int]) -> Bool {
+//     for i in 0..<sequence.count {
+//         var sum = 0
+//         for j in 0..<(sequence.count - i) {
+//             sum += sequence[i + j]
+//             if signMatrix[i][j] == "+" {
+//                 if sum <= 0 { return false }
+//                 continue
+//             } 
+            
+//             if signMatrix[i][j] == "-" {
+//                 if sum >= 0 { return false }
+//                 continue
+//             }
+
+//             if signMatrix[i][j] == "0" {
+//                 if sum != 0 { return false }
+//                 continue
+//             }
+//         }
+//     }
+//     return true
+// }
+
+// func back_tracking(sequence: [Int], depth: Int) {
+//     if !result.isEmpty { return }
+    
+//     if sequence.count == n {
+//         result = sequence
+//         return
+//     }
+
+//     let numSign = signMatrix[depth].first!
+
+//     if numSign == "+" {
+//         for i in 1...10 {
+//             if !checkSignMatrix(sequence: sequence + [i]) { continue }
+//             back_tracking(sequence: sequence + [i], depth: depth + 1)
+//         }
+//         return
+//     }
+//     if numSign == "-" {
+//         for i in stride(from: -1, through: -10, by: -1) {
+//             if !checkSignMatrix(sequence: sequence + [i]) { continue }
+//             back_tracking(sequence: sequence + [i], depth: depth + 1)
+//         }
+//         return
+//     }
+//     if numSign == "0" {
+//         back_tracking(sequence: sequence + [0], depth: depth + 1)
+//         return
+//     }
+// }
+
+// back_tracking(sequence: [], depth: 0)
+// print(result.map { String($0) }.joined(separator: " "))
+
+// 개선
+import Foundation
+
 let n = Int(readLine()!)!
 let sign: [Character] = Array(readLine()!)
 var signMatrix: [[Character]] = [] 
 var curIndex = 0
-var result = [Int]()
+var sequence = Array(repeating: 0, count: n)
 
 for i in stride(from: n, through: 1, by: -1) {
-    signMatrix.append(Array(sign[curIndex..<curIndex + i]))
+    signMatrix.append(Array(repeating: Character("x"), count: n - i) + Array(sign[curIndex..<curIndex + i]))
     curIndex += i
 }
 
-func checkSignMatrix(sequence: [Int]) -> Bool {
-    for i in 0..<sequence.count {
-        var sum = 0
-        for j in 0..<(sequence.count - i) {
-            sum += sequence[i + j]
-            if signMatrix[i][j] == "+" {
-                if sum <= 0 { return false }
-                continue
-            } 
-            
-            if signMatrix[i][j] == "-" {
-                if sum >= 0 { return false }
-                continue
-            }
-
-            if signMatrix[i][j] == "0" {
-                if sum != 0 { return false }
-                continue
-            }
-        }
-    }
-    return true
-}
-
 func back_tracking(sequence: [Int], depth: Int) {
-    if !result.isEmpty { return }
-    
-    if sequence.count == n {
-        result = sequence
-        return
+    if depth == n {
+        print(sequence.map { String($0) }.joined(separator: " "))
+        exit(0)
     }
 
-    let numSign = signMatrix[depth].first!
+    var candi = Set(-10...10)
+    for i in 0...depth {
+        let numSign = signMatrix[i][depth]
+        let prevSum = sequence[i..<depth].reduce(0, +)
 
-    if numSign == "+" {
-        for i in 1...10 {
-            if !checkSignMatrix(sequence: sequence + [i]) { continue }
-            back_tracking(sequence: sequence + [i], depth: depth + 1)
+        if numSign == "+" {
+            candi = candi.filter { $0 > -prevSum }
+        } else if numSign == "-" {
+            candi = candi.filter { $0 < -prevSum }
+        } else if numSign == "0" {
+            candi = candi.filter { $0 == -prevSum }
         }
-        return
     }
-    if numSign == "-" {
-        for i in stride(from: -1, through: -10, by: -1) {
-            if !checkSignMatrix(sequence: sequence + [i]) { continue }
-            back_tracking(sequence: sequence + [i], depth: depth + 1)
-        }
-        return
-    }
-    if numSign == "0" {
-        back_tracking(sequence: sequence + [0], depth: depth + 1)
-        return
+
+    for c in candi {
+        back_tracking(sequence: sequence + [c], depth: depth + 1)
     }
 }
 
 back_tracking(sequence: [], depth: 0)
-print(result.map { String($0) }.joined(separator: " "))
 
 // 맞춰봐
